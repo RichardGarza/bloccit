e44e5b1afaab2206bf043a50543aa83e65348142
Richard <rgactr@gmail.com>
2019-06-05 12:18:58 -0700

    Refactored code to make it more readable and DRY, changed description in test to more accurately describe the test.

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -2,48 +2,26 @@ const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
 const marco = 'http://localhost:3000/marco'
+
 describe("routes : static", () => {
 
-//#1
   describe("GET /", () => {
 
-//#2
     it("should return status code 200", (done) => {
-
-//#3
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
-
-//#4
         done();
       });
     });
-
   });
 
-  describe("Marco!! .... Polo!!!", () => {
-
-    //#2
-        it("should return status code 200", (done) => {
-    
-    //#3
-          request.get(marco, (err, res, body) => {
-            expect(res.statusCode).toBe(200);
-    
-    //#4
-            done();
-          });
-        });
-    
-        it("should return polo", (done) => {
-    
-          //#3
-                request.get(marco, (err, res, body) => {
-                  expect(res.body).toBe("polo");
-          
-          //#4
-                  done();
-                });
-              });
+  describe("GET /marco", () => {
+    it("should return status code 200 & should return 'polo' in the body of the response", (done) => {
+      request.get(marco, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toBe("polo");
+        done();
       });
+    });
+  });
 });
