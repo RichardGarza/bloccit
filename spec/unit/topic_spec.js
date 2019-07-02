12d537a0e23b0bb384aeb9fb9f98fcd3236f0970
Richard <rgactr@gmail.com>
2019-07-02 12:17:25 -0700

    Fixed topic-spec file I accidentally deleted.

--- spec/unit/topic_spec.js
+++ spec/unit/topic_spec.js
@@ -1,35 +1,43 @@
 const sequelize = require("../../src/db/models/index").sequelize;
 const Topic = require("../../src/db/models").Topic;
 const Post = require("../../src/db/models").Post;
+const User = require("../../src/db/models").User;
 
 describe("Post", () => {
   beforeEach((done) => {
     this.topic;
     this.post;
-    this.altPost;
+    this.user;
+
     sequelize.sync({force: true}).then((res) => {
-//Create a test Topic & set it to global object property
-      Topic.create({
-        title: "Expeditions to Alpha Centauri",
-        description: "A compilation of reports from recent visits to the star system."
+
+      User.create({
+        email: "starman@tesla.com",
+        password: "Trekkie4lyfe"
       })
-      .then((topic) => {
-        this.topic = topic;
-//Create a test Post & set it to global object property w/ assosiated
-//topic set to test Topic from above.
-        Post.create({
-          title: "My first visit to Proxima Centauri b",
-          body: "I saw some rocks.",
-          topicId: this.topic.id
+      .then((user) => {
+        this.user = user; //store the user
+
+        Topic.create({
+          title: "Expeditions to Alpha Centauri",
+          description: "A compilation of reports from recent visits to the star system.",
+
+          posts: [{
+            title: "My first visit to Proxima Centauri b",
+            body: "I saw some rocks.",
+            userId: this.user.id
+          }]
+        }, {
+          include: {
+            model: Post,
+            as: "posts"
+          }
         })
-        .then((post) => {
-          this.post = post;
+        .then((topic) => {
+          this.topic = topic; //store the topic
+          this.post = topic.posts[0]; //store the post
           done();
         });
-      })
-      .catch((err) => {
-        console.log(err);
-        done();
       });
     });
   });
@@ -75,4 +83,4 @@ describe("Post", () => {
       });
     });
   });
-});
+}); 
